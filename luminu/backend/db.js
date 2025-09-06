26533cb512733c1c857dc53f7c1c73d4b8990de3
Eric <ericalan0906@gmail.com>
2025-09-05 22:59:48 -0300

    1.0

--- luminu/backend/db.js
+++ luminu/backend/db.js
@@ -2,22 +2,28 @@ import dotenv from 'dotenv';
 dotenv.config();
 import { Sequelize } from 'sequelize';
 
-// ✅ Use a URL completa do Railway
-const sequelize = new Sequelize(process.env.MYSQL_PUBLIC_URL, {
-  dialect: 'mysql',
-  logging: false,
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 60000,
-    idle: 60000
-  },
-  dialectOptions: {
-    connectTimeout: 60000,
-    acquireTimeout: 60000,
-    timeout: 60000,
+const sequelize = new Sequelize(
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
+  {
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    dialect: 'mysql',
+    logging: false,
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 60000,
+      idle: 60000
+    },
+    dialectOptions: {
+      connectTimeout: 60000,
+      acquireTimeout: 60000,
+      timeout: 60000,
+    }
   }
-});
+);
 
 sequelize.authenticate()
   .then(() => console.log('✅ Conectado ao MySQL com Sequelize!'))
